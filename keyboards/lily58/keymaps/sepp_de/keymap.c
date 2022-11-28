4c0e4dd9b6d547b9ea41289703a8da8dea444435
Sebastian Stumpf <git@perlhacker.org>
2022-11-28 22:41:06 +0100

    more changes

--- keyboards/lily58/keymaps/sepp_de/keymap.c
+++ keyboards/lily58/keymaps/sepp_de/keymap.c
@@ -27,69 +27,82 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Z  |   U  |   I  |   O  |   P  |  Ü   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
-   |LShift|   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   Ö  |  Ä   |
- * |------+------+------+------+------+------|   +   |    |   #   |------+------+------+------+------+------|
- * |LCTRL |   Y  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   -  |RShift|
+   | Caps |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   Ö  |  Ä   |
+ * |------+------+------+------+------+------| BackSp|    | Delete|------+------+------+------+------+------|
+ * |LShift|   Y  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   -  |RShift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE |BackSP| RGUI |
+ *                   |LCtrl | LGui |LAlt  | /LOWER  /       \RAISE \  |Space |Enter | RAlt |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
- *
  */
 
  [_QWERTZ] = LAYOUT(
   KC_ESC,   DE_1,   DE_2,    DE_3,    DE_4,    DE_5,                     DE_6,    DE_7,    DE_8,    DE_9,    DE_0,    DE_SS,
   KC_TAB,   DE_Q,   DE_W,    DE_E,    DE_R,    DE_T,                     DE_Z,    DE_U,    DE_I,    DE_O,    DE_P,    DE_UDIA,
-  KC_LSFT,  DE_A,   DE_S,    DE_D,    DE_F,    DE_G,                     DE_H,    DE_J,    DE_K,    DE_L,    DE_ODIA, DE_ADIA,
-  KC_LCTRL, DE_Y,   DE_X,    DE_C,    DE_V,    DE_B, DE_PLUS,  DE_HASH,  DE_N,    DE_M,    DE_COMM, DE_DOT,  DE_MINS, KC_RSFT,
-                        KC_LALT, KC_LGUI, MO(_LOWER), KC_SPC, KC_ENT, MO(_RAISE), KC_BSPC, KC_RGUI
+  KC_CAPS,  DE_A,   DE_S,    DE_D,    DE_F,    DE_G,                     DE_H,    DE_J,    DE_K,    DE_L,    DE_ODIA, DE_ADIA,
+  KC_LSFT,  DE_Y,   DE_X,    DE_C,    DE_V,    DE_B, KC_BSPC,  KC_DEL,   DE_N,    DE_M,    DE_COMM, DE_DOT,  DE_MINS, KC_RSFT,
+                       KC_LCTRL, KC_LGUI, KC_LALT, MO(_LOWER), MO(_RAISE), KC_SPC, KC_ENTER, KC_RALT
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |                    |  F7  |  F8  |  F9  | F10  | F11  | F12  |
+ * |      |      |      |  MS1 |      |  MS2 |                    |      | PGUP |      |      |      |Insert|
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |      |      |      |      |      |                    |  ^   |   (  |   )  |      |      |      |
+ * |      |      |      |      |  MSU |      |                    |      |  Up  |      |      |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |      |      |      |      |      |-------.    ,-------|  `   |   [  |   ]  |      |      |      |
- * |------+------+------+------+------+------|   <   |    |    >  |------+------+------+------+------+------|
- * |      |      |      |      |      |      |-------|    |-------|      |   {  |   }  |      |      |      |
+ * |      |      |      |  MSL |  MSD |  MSR |-------.    ,-------| Left | Down | Right|      |      |      |
+ * |------+------+------+------+------+------| Home  |    |  End  |------+------+------+------+------+------|
+ * |      |      |      |      |      |      |-------|    |-------|      | PGDN |      |      |      |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE |  Del | RGUI |
- *                   |      |      |      |/       /         \      \ |      |      |      |
- *                   `----------------------------'           '------''--------------------'
- */
-[_LOWER] = LAYOUT(
-  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   DE_CIRC, DE_LPRN, DE_RPRN, XXXXXXX, XXXXXXX, XXXXXXX,
-  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   DE_GRV,  DE_LBRC, DE_RBRC, XXXXXXX, XXXXXXX, XXXXXXX,
-  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, DE_LABK, DE_RABK, XXXXXXX, DE_LCBR, DE_RCBR, XXXXXXX, XXXXXXX, _______,
-                             _______, _______, _______, _______, _______,  _______, KC_DEL, _______
-),
-/* RAISE
- * ,-----------------------------------------.                    ,-----------------------------------------.
- * |Mprev |MplayP|Mnext |Calc  | VolD | VolU |                    |      |      |      |      |      |      |
- * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |      |  Up  |      |      | Home |                    |  End |      |      | MSU  |      |      |
- * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      | Left | Down |Right |      |      |-------.    ,-------|      |      | MSL  | MSD  | MSR  |      |
- * |------+------+------+------+------+------| PGUP  |    | PGDN  |------+------+------+------+------+------|
- * |      |      |      |      |      |Insert|-------|    |-------| MS1  | MS2  |      |      |      |      |
- * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE |BackSP| RGUI |
+ *                   |LCtrl | LGui |LAlt  | /LOWER  /       \RAISE \  |Space |Enter | RAlt |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
 
-[_RAISE] = LAYOUT(
-  KC_MPRV, KC_MPLY, KC_MNXT, KC_CALC,  KC_VOLD, KC_VOLU,                    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  XXXXXXX, XXXXXXX, KC_UP,   XXXXXXX,  XXXXXXX, KC_HOME,                    KC_END,  XXXXXXX, XXXXXXX, KC_MS_U, XXXXXXX, XXXXXXX,
-  _______, KC_LEFT, KC_DOWN, KC_RIGHT, XXXXXXX, XXXXXXX,                    XXXXXXX, XXXXXXX, KC_MS_L, KC_MS_D, KC_MS_R, XXXXXXX,
-  _______, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, KC_INS,   KC_PGUP, KC_PGDN, KC_BTN1, KC_BTN2, XXXXXXX, XXXXXXX, XXXXXXX, _______,
+[_LOWER] = LAYOUT(
+  XXXXXXX, XXXXXXX, XXXXXXX, KC_MS_BTN1, XXXXXXX, KC_MS_BTN2,                XXXXXXX, KC_PGUP, XXXXXXX,  XXXXXXX, XXXXXXX, KC_INS,
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    KC_MS_U, XXXXXXX,                   XXXXXXX, KC_UP,   XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX,
+  _______, XXXXXXX, XXXXXXX, KC_MS_L,    KC_MS_D, KC_MS_R,                   KC_LEFT, KC_DOWN, KC_RIGHT, XXXXXXX, XXXXXXX, XXXXXXX,
+  _______, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX,  KC_HOME, KC_END, XXXXXXX, KC_PGDN, XXXXXXX,  XXXXXXX, XXXXXXX, _______,
                              _______, _______, _______,  _______, _______, _______, _______, _______
 ),
+/* RAISE
+ * ,-----------------------------------------.                    ,-----------------------------------------.
+ * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |                    |  F7  |  F8  |  F9  | F10  | F11  | F12  |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |      |      |  +   |  (   |   )  |  ^   |                    |      |      |      |      |      |      |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |      |      |  #   |  [   |   ]  |  `   |-------.    ,-------|      |      |      |      |      |      |
+ * |------+------+------+------+------+------|   <   |    |    >  |------+------+------+------+------+------|
+ * |      |      |  |   |  {   |   }  |      |-------|    |-------|      |      |      |      |      |      |
+ * `-----------------------------------------/       /     \      \-----------------------------------------'
+ *                   |LCtrl | LGui |LAlt  | /LOWER  /       \RAISE \  |Space |Enter | RAlt |
+ *                   |      |      |      |/       /         \      \ |      |      |      |
+ *                   `----------------------------'           '------''--------------------'
+ */
+[_RAISE] = LAYOUT(
+  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
+  _______, XXXXXXX, DE_PLUS, DE_LPRN, DE_RPRN, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  _______, XXXXXXX, DE_HASH, DE_LBRC, DE_RBRC, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  _______, XXXXXXX, DE_PIPE, DE_LCBR, DE_RCBR, XXXXXXX, DE_LABK, DE_RABK, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
+                             _______, _______, _______, _______, _______,  _______, _______, _______
+),
+
+/*
+
+XXXXXXX, KC_PGUP, XXXXXXX,
+XXXXXXX, KC_UP,   XXXXXXX,
+KC_LEFT, KC_DOWN, KC_RIGHT
+XXXXXXX, KC_PGDN, KC_INS, 
+
+KC_MS_BTN1, XXXXXXX, KC_MS_BTN1, 
+XXXXXXX,    KC_MS_U, XXXXXXX,    
+KC_MS_L,    KC_MS_D, KC_MS_R,    
+XXXXXXX,    XXXXXXX, XXXXXXX,    
+
+*/
 /* ADJUST
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
+ * |MPrev |MPlay |MNext | Calc | VolD | VolU |                    |      |      |      |      |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |      |      |      |      | Run  |      |                    |      |      | RowM |      | Pull | Push |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
@@ -97,12 +110,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
  * |      |      |      |Commit|Checko|      |-------|    |-------|      |      |      |      |      |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE |BackSP| RGUI |
+ *                   |LCtrl | LGui |LAlt  | /LOWER  /       \RAISE \  |Space |Enter | RAlt |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
   [_ADJUST] = LAYOUT(
-  XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX,    XXXXXXX,      XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX,  XXXXXXX,
+  KC_MPRV, KC_MPLY, KC_MNXT,    KC_CALC,    KC_VOLD,      KC_VOLU,                   XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX,  XXXXXXX,
   XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX,    C(S(KC_F11)), XXXXXXX,                   XXXXXXX, XXXXXXX, LSA(KC_INS), XXXXXXX, GIT_PULL, GIT_PUSH,
   XXXXXXX, XXXXXXX, GIT_STATUS, GIT_DIFF,   XXXXXXX,      XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX,  XXXXXXX,
   XXXXXXX, XXXXXXX, XXXXXXX,    GIT_COMMIT, GIT_CHECKOUT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX,  XXXXXXX,
@@ -125,10 +138,6 @@ oled_rotation_t oled_init_user(oled_rotation_t rotation) {
 
 // When you add source files to SRC in rules.mk, you can use functions.
 const char *read_layer_state(void);
-const char *read_logo(void);
-void set_keylog(uint16_t keycode, keyrecord_t *record);
-const char *read_keylog(void);
-const char *read_keylogs(void);
 
 // const char *read_mode_icon(bool swap);
 // const char *read_host_led_state(void);
@@ -136,29 +145,27 @@ const char *read_keylogs(void);
 // const char *read_timelog(void);
 
 bool oled_task_user(void) {
-  if (is_keyboard_master()) {
-    // If you want to change the display of OLED, you need to change here
-    oled_write_ln(read_layer_state(), false);
-    oled_write_ln(read_keylog(), false);
-    oled_write_ln(read_keylogs(), false);
-    //oled_write_ln(read_mode_icon(keymap_config.swap_lalt_lgui), false);
-    //oled_write_ln(read_host_led_state(), false);
-    //oled_write_ln(read_timelog(), false);
-  } else {
-    oled_write(read_logo(), false);
-  }
+    oled_set_cursor(0,1);
+    uint8_t n = get_current_wpm();
+    char    wpm_counter[4];
+    wpm_counter[3] = '\0';
+    wpm_counter[2] = '0' + n % 10;
+    wpm_counter[1] = (n /= 10) % 10 ? '0' + (n) % 10 : (n / 10) % 10 ? '0' : ' ';
+    wpm_counter[0] = n / 10 ? '0' + n / 10 : ' ';
+    oled_write_P(PSTR("WPM: "), false);
+    oled_write(wpm_counter, false);
+    oled_set_cursor(0,3); {
+        oled_write_ln(read_layer_state(), false);
+    }
     return false;
 }
 #endif // OLED_ENABLE
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   if (record->event.pressed) {
-#ifdef OLED_ENABLE
-    set_keylog(keycode, record);
-#endif
     switch (keycode) {
         case GIT_PULL:
-            SEND_STRING("git pull -p\n");
+            SEND_STRING("git pull\n");
             break;
         case GIT_PUSH:
             SEND_STRING("git push\n");
@@ -177,7 +184,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             SEND_STRING("git checkout ");
             break;
     }
-    // set_timelog();
   }
   return true;
 }
--- keyboards/lily58/keymaps/sepp_de/rules.mk
+++ keyboards/lily58/keymaps/sepp_de/rules.mk
@@ -10,13 +10,8 @@ RGBLIGHT_ENABLE = no       # Enable WS2812 RGB underlight.
 SWAP_HANDS_ENABLE = no      # Enable one-hand typing
 OLED_ENABLE= yes     # OLED display
 DYNAMIC_MACRO_ENABLE = no
+WPM_ENABLE = yes
 LTO_ENABLE = yes
 
 # If you want to change the display of OLED, you need to change here
-SRC +=  ./lib/rgb_state_reader.c \
-        ./lib/layer_state_reader.c \
-        ./lib/logo_reader.c \
-        ./lib/keylogger.c \
-        # ./lib/mode_icon_reader.c \
-        # ./lib/host_led_state_reader.c \
-        # ./lib/timelogger.c \
+SRC +=  ./lib/layer_state_reader.c
