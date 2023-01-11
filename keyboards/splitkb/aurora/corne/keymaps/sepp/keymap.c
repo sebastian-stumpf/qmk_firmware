332acc19da23738ddb9f71c9f588abcc56bcf71c
Sebastian Stumpf <git@perlhacker.org>
2023-01-11 14:19:21 +0100

    added one handed copypasta codes

--- keyboards/splitkb/aurora/corne/keymaps/sepp/keymap.c
+++ keyboards/splitkb/aurora/corne/keymaps/sepp/keymap.c
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [FUN] = LAYOUT_split_3x6_3(
         _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       KC_F7,   KC_F8,   KC_F9,   KC_F12,  XXXXXXX,  _______,
         _______, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, XXXXXXX,       KC_F4,   KC_F5,   KC_F6,   KC_F11,  XXXXXXX,  XXXXXXX,
-        XXXXXXX, KC_ALGR, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       KC_F1,   KC_F2,   KC_F3,   KC_F10,  XXXXXXX,  _______,
+        XXXXXXX, KC_ALGR, C(DE_X), C(DE_C), C(DE_V), XXXXXXX,       KC_F1,   KC_F2,   KC_F3,   KC_F10,  XXXXXXX,  _______,
                                    _______, _______, _______,       _______,  _______, _______
     ),
     [NAV] = LAYOUT_split_3x6_3(
