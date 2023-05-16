9d17a7c2088168bbf2a93d978905ce2c50a27a25
Sebastian Stumpf <git@perlhacker.org>
2023-05-16 19:18:17 +0200

    added info.json / new keymap

--- keyboards/narfkb2/keymaps/default/keymap.c
+++ keyboards/narfkb2/keymaps/default/keymap.c
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     ),
     [NUM] = LAYOUT(
-        _______, XXXXXXX, XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,                                                       DE_7,    DE_8,    DE_9,    DE_TILD, XXXXXXX, _______,
+        _______, XXXXXXX, XXXXXXX,    C(KC_PGUP), C(KC_PGDN), XXXXXXX,                                                       DE_7,    DE_8,    DE_9,    DE_TILD, XXXXXXX, _______,
         _______, KC_LGUI, KC_LALT,    KC_LCTL,    KC_LSFT,    XXXXXXX, _______,                                     _______, DE_4,    DE_5,    DE_6,    DE_PIPE, XXXXXXX, XXXXXXX,
         XXXXXXX, XXXXXXX, C(S(DE_X)), C(S(DE_C)), C(S(DE_V)), XXXXXXX, _______, _______, _______, _______, _______, _______, DE_1,    DE_2,    DE_3,    DE_QUES, XXXXXXX, _______,
                                       _______,    _______, _______,                                                 _______, _______, DE_0
