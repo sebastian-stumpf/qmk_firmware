597ceae97354c8081caa55ed07e12e169907a578
Sebastian Stumpf <git@perlhacker.org>
2023-09-30 18:01:07 +0200

    cmini keymap

---
+++ keyboards/bastardkb/charybdis/3x6/keymaps/sepp/keymap.c
@@ -0,0 +1,113 @@
+#include QMK_KEYBOARD_H
+
+#include "keymap_german.h"
+
+enum charybdis_keymap_layers {
+    LAYER_BASE = 0,
+    LAYER_SYMBOLS,
+    LAYER_NUMERAL,
+    LAYER_FUNCTION,
+    LAYER_NAVIGATION,
+    LAYER_ADJUST,
+};
+
+
+#define A_LT LGUI_T(DE_A)
+#define S_LT LALT_T(DE_S)
+#define D_LT LCTL_T(DE_D)
+#define F_LT LSFT_T(DE_F)
+#define J_LT LSFT_T(DE_J)
+#define K_LT LCTL_T(DE_K)
+#define L_LT LALT_T(DE_L)
+#define HASH_LT LGUI_T(DE_HASH)
+#define MINS_LT LT(LAYER_ADJUST, DE_MINS)
+
+#define SPC_FUN LT(LAYER_FUNCTION, KC_SPC)
+#define L_SYM MO(LAYER_SYMBOLS)
+#define L_NAV MO(LAYER_NAVIGATION)
+#define L_NUM MO(LAYER_NUMERAL)
+
+#define OS_GUI OSM(MOD_LGUI)
+#define OS_ALT OSM(MOD_LALT)
+#define OS_CTL OSM(MOD_LCTL)
+#define OS_SFT OSM(MOD_LSFT)
+
+#define INS_MODE LSA(KC_INS)
+#define IJ_RUN C(S(KC_F11))
+
+enum combos {
+    RT_MB2,
+    FG_MB1,
+    VB_MB3
+};
+
+const uint16_t PROGMEM rt_combo[] = {DE_R, DE_T, COMBO_END};
+const uint16_t PROGMEM fg_combo[] = {F_LT, DE_G, COMBO_END};
+const uint16_t PROGMEM vb_combo[] = {DE_V, DE_B, COMBO_END};
+
+combo_t key_combos[] = {
+  [RT_MB2] = COMBO(rt_combo, KC_BTN2),
+  [FG_MB1] = COMBO(fg_combo, KC_BTN1),
+  [VB_MB3] = COMBO(vb_combo, KC_BTN3),
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [LAYER_BASE] = LAYOUT(
+    KC_ESC,  DE_Q,    DE_W,    DE_E,    DE_R,    DE_T,                   DE_Z,    DE_U,    DE_I,    DE_O,    DE_P,    KC_BSPC,
+    KC_TAB,  A_LT,    S_LT,    D_LT,    F_LT,    DE_G,                   DE_H,    J_LT,    K_LT,    L_LT,    HASH_LT, DE_PLUS,
+    DE_LABK, DE_Y,    DE_X,    DE_C,    DE_V,    DE_B,                   DE_N,    DE_M,    DE_COMM, DE_DOT,  MINS_LT, KC_ENT,
+                               L_NUM,   SPC_FUN, KC_ENT,                 L_NAV, L_SYM
+  ),
+  [LAYER_SYMBOLS] = LAYOUT(
+    _______, DE_AT,   DE_LPRN, DE_RPRN, DE_UDIA, DE_SS,                  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
+    _______, DE_BSLS, DE_LCBR, DE_RCBR, DE_ADIA, DE_ODIA,                XXXXXXX, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, XXXXXXX,
+    XXXXXXX, DE_EURO, DE_LBRC, DE_RBRC, DE_CIRC, DE_GRV,                 XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
+                               _______,  _______, _______,                _______, _______
+  ),
+  [LAYER_NUMERAL] = LAYOUT(
+    _______, XXXXXXX, XXXXXXX,    C(KC_PGUP), C(KC_PGDN), XXXXXXX,       DE_7,    DE_8,    DE_9,    DE_TILD, XXXXXXX, _______,
+    _______, KC_LGUI, KC_LALT,    KC_LCTL,    KC_LSFT,    XXXXXXX,       DE_4,    DE_5,    DE_6,    DE_PIPE, DE_LABK, XXXXXXX,
+    XXXXXXX, XXXXXXX, C(S(DE_X)), C(S(DE_C)), C(S(DE_V)), XXXXXXX,       DE_1,    DE_2,    DE_3,    DE_QUES, DE_PLUS, _______,
+                                  _______,    _______,    _______,       _______, DE_0
+  ),
+  [LAYER_FUNCTION] = LAYOUT(
+    _______, OS_GUI,  OS_ALT,  OS_CTL,  OS_SFT,  XXXXXXX,                KC_F7,    KC_F8,   KC_F9,   KC_F12,  TO(LAYER_BASE),    KC_DEL,
+    _______, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, XXXXXXX,                KC_F4,    KC_F5,   KC_F6,   KC_F11,  XXXXXXX,           XXXXXXX,
+    XXXXXXX, XXXXXXX, C(DE_X), C(DE_C), C(DE_V), XXXXXXX,                KC_F1,    KC_F2,   KC_F3,   KC_F10,  TG(LAYER_NUMERAL), _______,
+                               _______, _______, _______,                _______,  _______
+  ),
+  [LAYER_NAVIGATION] = LAYOUT(
+    _______, CW_TOGG, KC_PGUP, KC_HOME, KC_UP,   KC_END,                 XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
+    _______, KC_CAPS, KC_PGDN, KC_LEFT, KC_DOWN, KC_RIGHT,               XXXXXXX, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, XXXXXXX,
+    XXXXXXX, KC_INS,  INS_MODE,XXXXXXX, XXXXXXX, XXXXXXX,                XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
+                               _______, _______, _______,                _______, _______
+  ),
+  [LAYER_ADJUST] = LAYOUT(
+    _______, XXXXXXX, S_D_RMOD,DPI_RMOD,XXXXXXX, SNP_TOG,                XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
+    _______, XXXXXXX, S_D_MOD, DPI_MOD, XXXXXXX, DRG_TOG,                XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
+                               _______, _______, _______,                _______, _______
+  ),
+};
+
+bool caps_word_press_user(uint16_t keycode) {
+    switch (keycode) {
+        case KC_A ... KC_Z:
+        case KC_MINS:
+        case DE_MINS:
+        case DE_UDIA:
+        case DE_ADIA:
+        case DE_ODIA:
+            add_weak_mods(MOD_BIT(KC_LSFT));
+            return true;
+
+        case KC_1 ... KC_0:
+        case KC_BSPC:
+        case KC_DEL:
+        case KC_UNDS:
+            return true;
+
+        default:
+            return false;
+    }
+}
