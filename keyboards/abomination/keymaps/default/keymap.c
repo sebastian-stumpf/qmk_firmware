56c519fc8ef5e8cccc7fe56e6f5d24f9208b9ce2
Sebastian Stumpf <git@perlhacker.org>
2023-10-07 18:59:23 +0200

    added homerow mods to mouse layer

--- keyboards/abomination/keymaps/default/keymap.c
+++ keyboards/abomination/keymaps/default/keymap.c
@@ -101,9 +101,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [MOUSE] =  LAYOUT(
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, DRAG_SCROLL,                    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______,      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, XXXXXXX, _______, _______,      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
         XXXXXXX, C(DE_X), C(DE_C), C(DE_V), XXXXXXX, _______, _______,      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                          KC_BTN2, KC_BTN1, KC_BTN3,                        _______, _______, _______
+                          KC_BTN2, KC_BTN3, KC_BTN1,                        _______, _______, _______
     )
 };
 
